2e6527c3aa91808ab102c9288d5e155902036c68
Harish Natarajan <hyxnatarajan@gmail.com>
2016-09-28 15:38:59 +0200

    PD-3200 Fixed typo for ping http handler

--- http/http.go
+++ http/http.go
@@ -1,13 +1,17 @@
 package http
 
-import(
-  "github.com/Shop2market/goping/domain"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Shop2market/goping/domain"
+	"github.com/julienschmidt/httprouter"
 )
 
 func Ping(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	response := json.NewEncoder(w)
-	pingResult := domain.Ping(map[string]interface{})
+	pingResult := domain.Ping(map[string]interface{}{})
 
-  w.WriteHeader(200)
+	w.WriteHeader(200)
 	response.Encode(pingResult)
 }
